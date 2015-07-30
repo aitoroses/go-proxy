cc6b928232763231aa4764bfc2b0af86deae8b13
Aitor Oses <aitor.oses@gmail.com>
2015-07-30 13:30:55 +0200

    Server is proxying correctly

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import proxy "github.com/aitoroses/proxy/proxy"
+import (
+	"net/http"
+
+	proxy "github.com/aitoroses/proxy/proxy"
+)
 
 func main() {
 	svs, _ := proxy.Read("servers.json")
 	server := proxy.New(svs)
+
+	go http.ListenAndServe(":3000", nil)
+	go http.ListenAndServe(":7003", nil)
+
 	server.Start()
+
 }
--- proxy/proxy_server.go
+++ proxy/proxy_server.go
@@ -3,14 +3,38 @@ package proxy
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"regexp"
 	"strconv"
 )
 
-func hello(message string) func(w http.ResponseWriter, r *http.Request) {
+func proxyCall(domain string) func(w http.ResponseWriter, r *http.Request) {
+
+	// Create a HTTP client
+	client := &http.Client{}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, message)
+		// Make an Http call to the proxied route
+		url := domain + r.URL.Path
+		fmt.Printf("%s %s\n", r.Method, url)
+
+		// Create a Request
+		req, err := http.NewRequest(r.Method, url, nil)
+		if err != nil {
+			io.WriteString(w, "Error creating the request to "+url)
+		}
+
+		// Make http call to proxy
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+
+		// Write the response
+		body, err := ioutil.ReadAll(resp.Body)
+		w.Header().Set("X-proxy", "GrayProxy")
+		io.WriteString(w, string(body))
 	}
 }
 
@@ -19,14 +43,25 @@ type Proxy struct {
 	svs Servers
 }
 
+func (s *Proxy) getName(i int) string {
+	sv := s.svs.Servers[i]
+	return fmt.Sprintf("%s:%d", sv.Host, sv.Port)
+}
+
+func (s *Proxy) getPath(i int) string {
+	return fmt.Sprintf("http://%s", s.getName(i))
+}
+
 // Start the proxy server
 func (s *Proxy) Start() {
+
 	// Create a new RegexpHandler as mux
 	mux := &RegexpHandler{}
 	for i := 0; i < len(s.svs.Servers); i++ {
-		s := s.svs.Servers[i]
-		fmt.Println(s.Mount)
-		mux.HandleFunc(regexp.MustCompile("^"+s.Mount), hello(fmt.Sprintf("%s:%d", s.Host, s.Port)))
+		sv := s.svs.Servers[i]
+		fmt.Printf("Proxy: %s -> %s\n", sv.Mount, s.getPath(i))
+		// Add handler based on a regexp
+		mux.HandleFunc(regexp.MustCompile("^"+sv.Mount), proxyCall(s.getPath(i)))
 	}
 
 	// Start listening
